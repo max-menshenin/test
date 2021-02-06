7a6d4053e849c3ca244a2ad50ec2fe0cb7147ce9
postmind <max.menshenin.dev@yandex.ru>
2021-02-06 14:17:13 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+</head>
+<body>
+<?php
+if (isset($_POST['submit']))
+
+?>
+
+<form action="index.php" method="post">
+    <input name="data">
+    <br>
+    <button type="submit" class="btn btn-primary">Ok</button>
+</form>
+</div>
+</body>
+</html>
